7e2f706db82740b9e53020da451f14e65d30a8cb
junsu1794 <sphw7700@nate.com>
2024-01-31 18:16:30 +0900

    about img 변경

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', event => {
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
     );
-    
+
     responsiveNavItems.map(function (responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
